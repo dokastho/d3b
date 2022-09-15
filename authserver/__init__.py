13b7ccd0b4bb0b1b1140178be7c7baba84e6b793
Thomas Dokas <dokastho@umich.edu>
2022-09-15 16:24:09 +0000

    setup debugger

--- authserver/__init__.py
+++ authserver/__init__.py
@@ -3,7 +3,7 @@ import flask
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)  # pylint: disable=invalid-name
 # Read settings from config module (site/config.py)
-app.config.from_object('app.config')
+app.config.from_object('authserver.config')
 # Overlay settings read from a Python file whose path is set in the environment
 # variable SITE_SETTINGS. Setting this environment variable is optional.
 # Docs: http://flask.pocoo.org/docs/latest/config/
@@ -13,3 +13,4 @@ app.config.from_object('app.config')
 app.config.from_envvar('SITE_SETTINGS', silent=True)
 
 
+from authserver.views import show_index
