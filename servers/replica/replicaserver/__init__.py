816a8a0c4f7dee53764a5dc6d1a12089dbd40492
Thomas Dokas <tjdokas@gmail.com>
2023-04-25 18:36:45 +0000

    create replica server files

---
+++ servers/replica/replicaserver/__init__.py
@@ -0,0 +1,17 @@
+"""Package node initializer."""
+import flask
+# app is a single object used by all the code modules in this package
+app = flask.Flask(__name__)  # pylint: disable=invalid-name
+# Read settings from config module (site/config.py)
+app.config.from_object('replicaserver.config')
+# Overlay settings read from a Python file whose path is set in the environment
+# variable SITE_SETTINGS. Setting this environment variable is optional.
+# Docs: http://flask.pocoo.org/docs/latest/config/
+#
+# EXAMPLE:
+# $ export SITE_SETTINGS=secret_key_config.py
+app.config.from_envvar('SITE_SETTINGS', silent=True)
+
+
+from replicaserver.api import *
+from replicaserver.model import *
--- servers/replica/setup.py
+++ servers/replica/setup.py
@@ -1,17 +1,17 @@
 """
-Schema site python package configuration.
+Replica Database python webserver package configuration.
 """
 
 from setuptools import setup
 
 setup(
-    name='schemaserver',
+    name='replicaserver',
     version='0.1.0',
-    packages=['schemaserver'],
+    packages=['replicaserver'],
     author="Thomas Dokas",
     author_email="dokastho@umich.edu",
     url="https://github.com/dokastho/d3b",
-    description="A schema maintenance server for webserver db's.",
+    description="An Authentication server for web based API's.",
     long_description_content_type="text/markdown",
     include_package_data=True,
     install_requires=[
