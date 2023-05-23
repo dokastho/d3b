dca75490393557de876a25ddde76712fe5a16c0e
Thomas Dokas <tjdokas@gmail.com>
2023-05-23 01:48:17 +0000

    Allow dynamically sized args

--- servers/replica/replicaserver/rpcs.py
+++ servers/replica/replicaserver/rpcs.py
@@ -1,8 +1,9 @@
 from random import randint
 
 QUERY_LEN = 128
-NARGS = 7
-ARG_LEN = 16
+ARG_LEN = 96
+MISC_LEN = 32
+MISC_ITEM_LEN = 8
 
 
 class d3b_op:
@@ -11,40 +12,41 @@ class d3b_op:
     query: str
     args: 'list[str]'
 
-    def __init__(self, table: str = "", query: str = "", *args) -> None:
+    def __init__(self, table: str = "", query: str = "", args=[]) -> None:
         self.table = table
         self.query = query
-        self.args = [""] * NARGS
+        self.args = args
         self.seed = randint(0, 0xffffffff)
 
-        if len(table) > ARG_LEN:
+        if len(table) > MISC_ITEM_LEN:
             raise Exception("Invalid table length")
 
-        if len(args) > NARGS:
-            raise Exception("Invalid nargs in query")
-        for i in range(len(args)):
-            if len(args[i]) > ARG_LEN:
-                raise Exception("Query argument is of invalid length")
-            self.args[i] = args[i]
+        s = 0
+        for arg in args:
+            s += len(arg)
             pass
+        if s > ARG_LEN:
+            raise Exception("Too many args in query")
         pass
 
     def __bytes__(self) -> bytes:
         bb = self.seed.to_bytes(4, "little")
+        # encode metadata (table + arg lengths)
+        misc_len = 0
         bb += self.table.encode("ascii")
-        bb += b"\x00" * (ARG_LEN - len(self.table))
+        misc_len += len(self.table)
+        bb += b"\x00" * (MISC_ITEM_LEN - len(self.table))
+        for arg in self.args:
+            bb += len(arg).to_bytes(1, "little")
+            misc_len += 1
+            pass
+        bb += b"\x00" * (ARG_LEN - len(misc_len))
+
+        # data payload
         bb += self.query.encode("ascii")
         bb += b"\x00" * (QUERY_LEN - len(self.query))
-        if len(self.args) != NARGS:
-            raise Exception("Invalid nargs in query")
         for arg in self.args:
-
-            # ensure valid argument length
-            if len(arg) > ARG_LEN:
-                raise Exception("Query argument is of invalid length")
-
             bb += arg.encode("ascii")
-            bb += b"\x00" * (ARG_LEN - len(arg))
 
         return bb
 
@@ -53,30 +55,29 @@ class d3b_op:
         stop = 4
         self.seed = int.from_bytes(data[start:stop], "little")
         start = stop
-        stop += ARG_LEN
+        stop += MISC_ITEM_LEN
         self.table = data[start:stop].decode("ascii")
+
+        # args
+        arg_lengths = []
+        for i in range(MISC_ITEM_LEN):
+            start = stop
+            stop += 1
+            arg_lengths.append(data[start])
+            pass
+
         start = stop
         stop += QUERY_LEN
         self.query = data[start:stop].decode("ascii")
-        for i in range(NARGS):
+        for n in arg_lengths:
             start = stop
-            stop += ARG_LEN
+            stop += n
             self.args[i] = data[start:stop].decode("ascii")
             pass
 
         # remove null bytes
         self.query = self.query.replace('\x00', '')
         self.table = self.table.replace('\x00', '')
-        
-        args: 'list[str]' = []
-
-        for arg in self.args:
-            arg = arg.replace('\x00', '')
-            if arg != '':
-                args.append(arg)
-                pass
-            pass
-        self.args = args
         pass
 
     pass
